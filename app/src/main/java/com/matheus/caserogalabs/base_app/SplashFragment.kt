5aa12c6443cabf4b812998af1e5badaba9783b58
Matheus <contact.matheus.mendes@gmail.com>
2021-07-20 16:52:13 -0300

    navigation implemented

--- app/src/main/java/com/matheus/caserogalabs/base_app/SplashFragment.kt
+++ app/src/main/java/com/matheus/caserogalabs/base_app/SplashFragment.kt
@@ -1,10 +1,13 @@
 package com.matheus.caserogalabs.base_app
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.matheus.caserogalabs.R
 import com.matheus.caserogalabs.databinding.FragmentSplashBinding
 
@@ -21,4 +24,15 @@ class SplashFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                findNavController().navigate(
+                    SplashFragmentDirections.actionSplashFragmentToUserPostFragment()
+                )
+            }, 1000
+        )
+    }
 }
---
+++ app/src/main/java/com/matheus/caserogalabs/feature/comment_user_post/CommentUserPostFragment.kt
@@ -0,0 +1,24 @@
+package com.matheus.caserogalabs.feature.comment_user_post
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.matheus.caserogalabs.R
+import com.matheus.caserogalabs.databinding.FragmentCommentUserPostBinding
+
+class CommentUserPostFragment : Fragment() {
+
+    private lateinit var binding: FragmentCommentUserPostBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCommentUserPostBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
